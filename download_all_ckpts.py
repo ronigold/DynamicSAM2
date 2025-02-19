1b6926503e6e9f4dd67932496ee35c0cbd841ede
Roni Goldshmidt <roni.goldshmidt@getnexar.com>
2025-02-19 09:06:03 +0000

    add download_all_ckpts

---
+++ download_all_ckpts.py
@@ -0,0 +1,13 @@
+import subprocess
+import os
+
+def run_script(directory, script_name):
+    script_path = os.path.join(directory, script_name)
+    if os.path.exists(script_path):
+        subprocess.run(["bash", script_name], cwd=directory, check=True)
+    else:
+        print(f"Script {script_path} not found!")
+
+# Run the scripts in their respective directories
+run_script('checkpoints', 'download_ckpts.sh')
+run_script('gdino_checkpoints', 'download_ckpts.sh')
